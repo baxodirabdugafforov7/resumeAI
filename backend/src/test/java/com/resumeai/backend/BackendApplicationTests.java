f4df86e3f3ec757c75f709dbc9a1b4f415513afa
abdugafforovbaxodir2005@gmail.com <baxodirabdugafforov7>
2025-07-24 15:24:14 +0500

    test-backend

--- backend/src/test/java/com/resumeai/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/resumeai/backend/BackendApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
-
+//	test to check github backend
 	@Test
 	void contextLoads() {
 	}
